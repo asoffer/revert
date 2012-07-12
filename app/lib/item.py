8598fb8c378af0823d05b2c4b89e4b04bbee3aac
Andy <andy@s231-225.resnet.ucla.edu>
2012-07-11 19:23:04 -0700

    key structure generalized to Item class. reverting made to work with the rotation

---
+++ app/lib/item.py
@@ -0,0 +1,40 @@
+from .thing import Thing
+
+from pandac.PandaModules import Point3, Vec3
+
+class Item(Thing):
+    """ 
+    The Item class is the base class for objects like keys, scrolls, etc... 
+    """
+
+
+    def __init__(self, model, loc = Point3()):
+        super(Item, self).__init__(model, loc = loc)
+
+        self.interval = None
+
+        self.setInterval(Point3())
+
+    def setModel(self, m):
+        #this is a faster way to keep track of the rotation... don't save and revert the intervals. just make a new one starting at the same rotation
+        self.interval.finish()
+
+        self.model.detachNode()
+        self.model = m
+        self.model.reparentTo(base.render)
+
+        #model properties are preserved, because we just keep the model. it's okay to do this, because this item won't have any body attached to it
+        self.setInterval(self.model.getHpr())
+
+    def setRot(self, rot):
+        self.model.setHpr(rot)
+
+    def getRot(self):
+        return self.model.getHpr()
+
+    def setInterval(self, startRot):
+        self.interval = self.model.hprInterval(6, startRot + Vec3(0, 0, 360), startHpr = startRot)
+        self.interval.loop()
+
+
+
--- app/objects/key.py
+++ app/objects/key.py
@@ -1,7 +1,7 @@
 from pandac.PandaModules import Point3
-from ..lib.thing import Thing
+from ..lib.item import Item
 
-class Key(Thing):
+class Key(Item):
     def __init__(self, loc = Point3()):
         """
         the location is the bottom-left corner of the platform
@@ -9,6 +9,4 @@ class Key(Thing):
 
         super(Key, self).__init__("key", loc)
 
-        self.interval = self.model.hprInterval(5, Point3(0, 0, 360), startHpr = Point3(0,0,0))
         self.model.setScale(0.5)
-        self.interval.loop()
